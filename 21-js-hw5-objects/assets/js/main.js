e54415ea21abbe4fa6db649053980e9e456f1dee
Ponomarenko Volodymyr <vol.ponomarenko@gmail.com>
2025-08-17 18:49:52 +0300

    added task 2 time

--- 21-js-hw5-objects/assets/js/main.js
+++ 21-js-hw5-objects/assets/js/main.js
@@ -97,6 +97,87 @@ function roadCalc(){
     }
 }
 
+/*Task 2. Створити об'єкт, що описує час (години, хвилини, секунди), і такі функції для роботи з цим об'єктом:
+Для виведення часу на екран.
+Зміни часу на передану кількість секунд.
+Зміни часу на передану кількість хвилин.
+Зміни часу на передану кількість годин.
+Враховуйте, що в останніх 3-х функціях, при зміні однієї 
+частини часу, може змінитися і інша. Наприклад: якщо до
+часу «20:59:45» додати 30 секунд, то повинно вийти «21:00:15»,
+а не «20:59:75». Також потрібно передбачити можливість того,
+що користувач може передати 150 секунд, або 75 хвилин.*/
+
+const time = {
+    h:0,
+    m:0,
+    s:0
+}
+
+let timeInSec = 0
+const rez4 = document.getElementById("rez4")
+
+
+const min = +document.getElementById("mins").value
+const sec = +document.getElementById("sec").value
+ 
+function time2sec (time) {
+     timeInSec = time.h*3600 + time.m*60 + time.s
+}
+    
+function sec2time (timeInSec){
+        time.h = Math.floor(timeInSec / 3600)
+        time.m = Math.floor((timeInSec - time.h * 3600) / 60)
+        time.s = timeInSec - (time.h * 3600 + time.m * 60)
+}
+ 
+function showTime(){
+   return rez4.innerHTML = `${String(time.h).padStart(2, '0')}:${String(time.m).padStart(2, '0')}:${String(time.s).padStart(2, '0')}`
+
+}
+
+function changeTime(delta){
+    time2sec(time)
+    timeInSec += delta
+    sec2time(timeInSec)
+    showTime()
+}
+    
+function addHour(){
+       const hours = +document.getElementById("hours").value       
+       changeTime(hours * 3600)
+      
+}    
+
+function addMins(){
+       const min = +document.getElementById("min").value
+       changeTime(min * 60)
+
+
+}
+
+function addSec(){
+       const sec = +document.getElementById("sec").value       
+       changeTime(sec)     
+}
+
+function minusHour(){
+       const hours = +document.getElementById("hours").value
+       changeTime((-hours) * 3600)
+       
+}    
+
+function minusMins(){
+       const min = +document.getElementById("min").value       
+       changeTime((- min) * 60)
+       
+}
+
+function minusSec(){
+       const sec = +document.getElementById("sec").value       
+       changeTime(-sec)
+}
+      
 
 
 
